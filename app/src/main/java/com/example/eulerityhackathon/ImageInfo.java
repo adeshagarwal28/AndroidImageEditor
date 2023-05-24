6ff89345c206e7226100785829c175fda3ca3e4c
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-05-24 20:58:02 +0530

    Data class

---
+++ app/src/main/java/com/example/eulerityhackathon/ImageInfo.java
@@ -0,0 +1,13 @@
+package com.example.eulerityhackathon;
+
+import android.app.Application;
+import android.net.Uri;
+
+import java.io.File;
+
+public class ImageInfo extends Application {
+
+    public static String originalUrl;
+    public static File editedImage;
+    public static Uri imageUri;
+}
